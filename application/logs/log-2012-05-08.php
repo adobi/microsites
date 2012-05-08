52f0f7fc1cb3c028b8077920805426c36040246b
Attila Dobi <hello@adobi.hu>
2012-05-08 13:43:24 +0200

    fix

---
+++ application/logs/log-2012-05-08.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-05-08 11:23:34 --> Query error: Table 'microsites.im_site' doesn't exist
+ERROR - 2012-05-08 11:24:06 --> Query error: Table 'microsites.im_store_type' doesn't exist
+ERROR - 2012-05-08 11:25:15 --> Severity: Notice  --> Undefined index: videos F:\wamp\www\microsites\application\models\microsites.php 68
